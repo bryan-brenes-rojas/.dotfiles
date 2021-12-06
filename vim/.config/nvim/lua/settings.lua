3507562a8113ed40730a016d13035e37bda8aa18
Bryan Brenes <bryanbrenes1@gmail.com>
2021-12-06 07:55:20 -0600

    highlight text on yank nvim

--- vim/.config/nvim/lua/settings.lua
+++ vim/.config/nvim/lua/settings.lua
@@ -31,3 +31,10 @@ vim.opt.cindent = true
 
 -- global variables
 vim.g.mapleader = " "
+
+vim.cmd([[
+augroup highlight_yank
+    autocmd!
+    au TextYankPost * silent! lua vim.highlight.on_yank{higroup="IncSearch", timeout=300}
+augroup END
+]])
