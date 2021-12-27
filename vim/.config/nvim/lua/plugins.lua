ad436628e7d921c6f239eb44dc4e43f2b6e8e12b
Bryan Brenes <bryanbrenes1@gmail.com>
2021-12-27 11:41:40 -0600

    Install treesitter

--- vim/.config/nvim/lua/plugins.lua
+++ vim/.config/nvim/lua/plugins.lua
@@ -27,4 +27,8 @@ return require('packer').startup(function()
     'nvim-telescope/telescope.nvim',
     requires = { {'nvim-lua/plenary.nvim'} }
   }
+	use {
+		'nvim-treesitter/nvim-treesitter',
+		run = ':TSUpdate'
+	}
 end)
---
+++ vim/.config/nvim/lua/treesitter_config.lua
@@ -0,0 +1,11 @@
+require( 'nvim-treesitter.configs' ).setup {
+  ensure_installed = 'maintained',
+
+  -- Install languages synchronously (only applied to `ensure_installed`)
+  sync_install = false,
+
+  -- highlight = {
+    -- enable = true,
+    -- additional_vim_regex_highlighting = false,
+  -- },
+}
