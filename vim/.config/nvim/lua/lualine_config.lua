3c7e211faab75374db812b922183cc648f70857c
Bryan Brenes <bryanbrenes1@gmail.com>
2022-04-02 17:51:50 -0600

    Update lualine config

--- vim/.config/nvim/lua/lualine_config.lua
+++ vim/.config/nvim/lua/lualine_config.lua
@@ -10,9 +10,16 @@ require('lualine').setup {
   },
   sections = {
     lualine_a = {'mode'},
-    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_b = {
+			'branch', 
+			'diff', 
+			{
+				'diagnostics',
+				sections = { 'error' },
+			}
+		},
     lualine_c = {'filename'},
-    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_x = {'filetype'},
     lualine_y = {'progress'},
     lualine_z = {'location'}
   },
