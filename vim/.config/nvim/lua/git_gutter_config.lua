12f4bebe8a697dc26c02e8d7a111a8c98dffadfe
Bryan Brenes <bryanbrenes1@gmail.com>
2022-04-10 18:41:07 -0600

    Git gutter mappings

---
+++ vim/.config/nvim/lua/git_gutter_config.lua
@@ -0,0 +1,4 @@
+local opts = {noremap = true, silent = true}
+vim.api.nvim_set_keymap('n',']h', ':GitGutterNextHunk<CR>', opts)
+vim.api.nvim_set_keymap('n','[h', ':GitGutterPrevHunk<CR>', opts)
+vim.api.nvim_set_keymap('n','<leader>hp', ':GitGutterPreviewHunk<CR>', opts)
