2c05dfd5bd4f4fc323fef147f6651b46bb9257ea
Bryan Brenes <bryanbrenes1@gmail.com>
2022-12-16 21:47:33 -0600

    Add typescript relative import preference and git_files telescope_config

--- vim/.config/nvim/lua/telescope_config.lua
+++ vim/.config/nvim/lua/telescope_config.lua
@@ -3,6 +3,7 @@ local options = { noremap = true, silent = true }
 vim.api.nvim_set_keymap('n', '<leader>p', ":lua require'telescope.builtin'.find_files()<CR>", options)
 vim.api.nvim_set_keymap('n', '<leader>b', ":lua require'telescope.builtin'.buffers()<CR>", options)
 vim.api.nvim_set_keymap('n', '<leader>s', ":lua require'telescope.builtin'.live_grep()<CR>", options)
+vim.api.nvim_set_keymap('n', '<leader>gg', ":lua require'telescope.builtin'.git_files()<CR>", options)
 
 require('telescope').setup({
   defaults = {
