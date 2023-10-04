0c8f347aa00231c95aa722b5e8b77e9255393c87
Bryan Brenes <bryanbrenes1@gmail.com>
2023-10-03 19:10:49 -0600

    Remove document highlight

--- vim/.config/nvim/lua/luasnip_config.lua
+++ vim/.config/nvim/lua/luasnip_config.lua
@@ -14,10 +14,10 @@ function _G.jumpNext()
 end
 
 -- keymaps for jumping
-vim.api.nvim_set_keymap('i', '<c-h>', '<Cmd>lua jumpPrev()<CR>', { silent = true })
-vim.api.nvim_set_keymap('v', '<c-h>', '<Cmd>lua jumpPrev()<CR>', { silent = true })
-vim.api.nvim_set_keymap('i', '<c-l>', '<Cmd>lua jumpNext()<CR>', { silent = true })
-vim.api.nvim_set_keymap('v', '<c-l>', '<Cmd>lua jumpNext()<CR>', { silent = true })
+-- vim.api.nvim_set_keymap('i', '<c-h>', '<Cmd>lua jumpPrev()<CR>', { silent = true })
+-- vim.api.nvim_set_keymap('v', '<c-h>', '<Cmd>lua jumpPrev()<CR>', { silent = true })
+-- vim.api.nvim_set_keymap('i', '<c-l>', '<Cmd>lua jumpNext()<CR>', { silent = true })
+-- vim.api.nvim_set_keymap('v', '<c-l>', '<Cmd>lua jumpNext()<CR>', { silent = true })
 
 local s = ls.snippet
 local sn = ls.snippet_node
@@ -30,28 +30,4 @@ local r = ls.restore_node
 
 ls.config.set_config({
   update_events = "TextChanged,TextChangedI",
-})
-
-ls.add_snippets("all", {
-  s("trigger", {
-    t({ "After jumping forward once, cursor is here ->" }), i(2, 'Second jump'),
-    t({ "", "After expanding, the cursor is here ->" }), i(1, 'First jump'),
-    t({ "", "After jumping once more, the snippet is exited there ->" }), i(0, 'last jump'),
-  }),
-})
-
--- javascript
-ls.add_snippets({"typescript", "javascript", "javascriptreact"}, {
-  s("function", {
-    i(1, 'public'),
-    t(' '),
-    i(2, 'name'),
-    t('('),
-    i(3, 'args'),
-    t('): '),
-    i(4, 'returnType'),
-    t({ ' {', '  ' }),
-    i(0),
-    t({ '', '}' }),
-  }),
 })
