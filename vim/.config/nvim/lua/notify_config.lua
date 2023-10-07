de16ce5b2f7bf3e4d5421d4be0d5a463940d4b6f
Bryan Brenes <bryanbrenes1@gmail.com>
2023-10-07 08:41:45 -0600

    Configure notifications and update which key config

--- vim/.config/nvim/lua/notify_config.lua
+++ vim/.config/nvim/lua/notify_config.lua
@@ -1,1 +1,12 @@
-vim.notify = require("notify")
+local notify = require("notify")
+
+notify.setup({
+	max_width = 70,
+	render = "wrapped-compact",
+	top_down = false,
+})
+
+vim.notify = notify
+
+vim.keymap.set("n", "<leader>nd", notify.dismiss, { noremap = true, silent = true, desc = "Notifications dismiss" })
+vim.keymap.set("n", "<leader>ns", ":Notifications<CR>", { noremap = true, silent = true, desc = "Show notification" })
