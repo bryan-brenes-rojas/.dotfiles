92dbcd64b3968a1860cc1e3045b3237c4cdc74c3
Bryan Brenes <bryanbrenes1@gmail.com>
2024-12-06 11:24:29 -0600

    Add highlighter for colors

---
+++ lazyvim/.config/nvim/lua/plugins/highlight-colors.lua
@@ -0,0 +1,8 @@
+return {
+  "brenoprata10/nvim-highlight-colors",
+  lazy = true,
+  ft = { "css", "scss", "typescript", "typescriptreact", "javascript" },
+  opts = {
+    render = "virtual",
+  },
+}
