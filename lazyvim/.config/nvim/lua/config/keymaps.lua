db074793b773a8af8b1166f76c420f97ea081e42
Bryan Brenes <bryanbrenes1@gmail.com>
2024-12-11 22:32:39 -0600

    Handle clipboard

--- lazyvim/.config/nvim/lua/config/keymaps.lua
+++ lazyvim/.config/nvim/lua/config/keymaps.lua
@@ -2,3 +2,7 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("n", "<leader>cv", "<cmd>let @+=@%<cr>", { desc = "Copy file path", silent = true })
+
+-- clipboard
+vim.keymap.set("n", "<leader>yy", '"+yy', { desc = "Copy to clipboard", silent = true })
+vim.keymap.set("v", "<leader>y", '"+y', { desc = "Copy to clipboard", silent = true })
--- lazyvim/.config/nvim/lua/config/options.lua
+++ lazyvim/.config/nvim/lua/config/options.lua
@@ -3,3 +3,4 @@
 -- Add any additional options here
 
 vim.wo.colorcolumn = "81"
+vim.opt.clipboard = ""
