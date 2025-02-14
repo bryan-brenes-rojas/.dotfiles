426bc0fa8d1429c7b4258d73b00a0b5e5ec4871c
Bryan Brenes <bryanbrenes1@gmail.com>
2025-02-13 20:17:48 -0600

    add quickfix list keymaps

--- lazyvim/.config/nvim/lua/config/keymaps.lua
+++ lazyvim/.config/nvim/lua/config/keymaps.lua
@@ -6,3 +6,9 @@ vim.keymap.set("n", "<leader>cv", "<cmd>let @+=@%<cr>", { desc = "Copy file path
 -- clipboard
 vim.keymap.set("n", "<leader>yy", '"+yy', { desc = "Copy to clipboard", silent = true })
 vim.keymap.set("v", "<leader>y", '"+y', { desc = "Copy to clipboard", silent = true })
+
+-- quickfix list
+vim.keymap.set("n", "<leader>qo", "<cmd>copen<cr>", { desc = "Open the quickfix list", silent = true })
+vim.keymap.set("n", "<leader>qc", "<cmd>cclose<cr>", { desc = "Close the quickfix list", silent = true })
+vim.keymap.set("n", "<leader>qn", "<cmd>cnext<cr>", { desc = "Go to next quickfix item", silent = true })
+vim.keymap.set("n", "<leader>qp", "<cmd>cprevious<cr>", { desc = "Go to next quickfix item", silent = true })
